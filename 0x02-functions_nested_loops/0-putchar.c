274f311884bb8bc4d915913629beb55a2ab6ab2d
Angela Karanja <njerikaranja61@gmail.com>
2022-03-15 13:19:57 -0700

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main- prints _putchar
  * followed by a new line
@@ -8,5 +9,6 @@
 int main(void)
 {
 	printf("_putchar\n");
+
 	return (0);
 }
